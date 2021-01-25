aa823725cf63faf11c1ff47e511c737238668391
Abdelkarim <tijaniabdelkarime@gmail.com>
2021-01-25 19:28:09 +0100

    Integrating Online Shop Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
-import { ProductListComponent } from './components/product-list/product-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
+
 @NgModule({
   declarations: [
     AppComponent,
-      ProductListComponent
-   ],
+    ProductListComponent
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule
   ],
-  providers: [
-    ProductService
-  ],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
