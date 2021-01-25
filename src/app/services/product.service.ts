7f3e1c2d14b658653771871306857a069b4c5ca6
Abdelkarim <tijaniabdelkarime@gmail.com>
2021-01-26 00:21:37 +0100

    Fixed Cros problem, and added search for products by Category

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -13,7 +13,7 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) {}
   //get all products
-  getProductList(): Observable<Product[]> {
+  getProductList(categoryId: number): Observable<Product[]> {
     return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
       map(response => response._embedded.products)
     );
